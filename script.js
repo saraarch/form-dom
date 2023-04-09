e7e0710346803aa63ce410631f2381381a7053d9
saraarch <96234478+saraarch@users.noreply.github.com>
2023-04-09 13:59:54 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+// var div=document.createElement("div");
+// div.innerHTML="this is div"
+// document.body.append(div);
+// div.style.backgroundColor="green";
+function foo(){
+    var fname=document.getElementById("First name").value;
+    console.log(`First name:${fname}`);
+     var mname=document.getElementById("Middle name").value;
+     console.log(`Middle name:${mname}`);
+    var Lname=document.getElementById("Last name",).value;
+    console.log(`Last name:${Lname}`);
+    var email=document.getElementById("email").value;
+    console.log(`email:${email}`);
+    
+}
